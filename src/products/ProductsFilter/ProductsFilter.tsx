b39c93b82d539ac999aeedf6006afcb5022ee53b
Lajoui Zakariae <lajoui.zakariae.1@gmail.com>
2024-01-26 02:36:57 +0100

    filters

--- src/products/ProductsFilter/ProductsFilter.tsx
+++ src/products/ProductsFilter/ProductsFilter.tsx
@@ -23,8 +23,14 @@ export default function ProductsFilter() {
                 Filters
             </Button>
             {open ? (
-                <div className="fixed bottom-0 left-0 p-4 min-h-100 w-full border-none rounded-t-3xl bg-white">
-                    <div className="flex justify-end">
+                <div className="fixed bottom-0 left-0 px-4  pb-4 max-h-150 overflow-y-auto w-full border-none rounded-t-3xl bg-white">
+                    <div className="sticky bg-white top-0 left-0 w-full pt-4 flex justify-between mb-4">
+                        <Button
+                            variant="primary"
+                            onClick={() => clearFilters()}
+                        >
+                            Clear
+                        </Button>
                         <button
                             className="btn btn-sm btn-circle"
                             onClick={() => setOpen(false)}
@@ -33,7 +39,7 @@ export default function ProductsFilter() {
                         </button>
                     </div>
 
-                    <div className="flex flex-col space-y-2 mb-2">
+                    <div className="flex flex-col space-y-2 mb-4">
                         <h3 className="text-lg font-bold text-boxdark">
                             Sort By
                         </h3>
@@ -109,7 +115,7 @@ export default function ProductsFilter() {
                         </div>
                     </div>
 
-                    <div className="flex flex-col space-y-2 mb-2">
+                    <div className="flex flex-col space-y-2 mb-4">
                         <h3 className="text-lg font-bold text-boxdark">
                             Price Range
                         </h3>
@@ -119,14 +125,68 @@ export default function ProductsFilter() {
                                 type="text"
                                 className="input input-bordered w-full"
                                 placeholder="0.00"
+                                value={filters.price_from}
+                                onChange={(ev) =>
+                                    setFilters((prev) => ({
+                                        ...prev,
+                                        price_from: ev.target.value,
+                                    }))
+                                }
                             />
                             <span className="text-lg">To</span>
                             <input
                                 type="text"
                                 className="input input-bordered w-full"
                                 placeholder="0.00"
+                                value={filters.price_to}
+                                onChange={(ev) =>
+                                    setFilters((prev) => ({
+                                        ...prev,
+                                        price_to: ev.target.value,
+                                    }))
+                                }
                             />
                         </div>
+                    </div>
+
+                    <div className="flex flex-col space-y-2 mb-4">
+                        <h3 className="text-lg font-bold text-boxdark">
+                            Cost Range
+                        </h3>
+
+                        <div className="flex justify-between items-center gap-3">
+                            <input
+                                type="text"
+                                className="input input-bordered w-full"
+                                placeholder="0.00"
+                                value={filters.cost_from}
+                                onChange={(ev) =>
+                                    setFilters((prev) => ({
+                                        ...prev,
+                                        cost_from: ev.target.value,
+                                    }))
+                                }
+                            />
+                            <span className="text-lg">To</span>
+                            <input
+                                type="text"
+                                className="input input-bordered w-full"
+                                placeholder="0.00"
+                                value={filters.cost_to}
+                                onChange={(ev) =>
+                                    setFilters((prev) => ({
+                                        ...prev,
+                                        cost_to: ev.target.value,
+                                    }))
+                                }
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <button className="btn btn-primary w-full">
+                            Apply Filters
+                        </button>
                     </div>
                 </div>
             ) : null}
